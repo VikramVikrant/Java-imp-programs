6e606b2bf1db0d7b78e77806b529f799f30391fc
VikramVikrant <mensharetoo@gmail.com>
2020-07-12 19:41:30 +0530

    Adding single parameter lambda function

---
+++ singlepar_lambda.java
@@ -0,0 +1,16 @@
+
+interface Sayable{
+    public String say(String name);
+}
+
+public class singlepar_lambda {
+    public static void main(String[] args) {
+        Sayable s1 = (name) -> {
+            return "Welcome," + name;
+        };
+        System.out.println(s1.say("Mr.Stark"));
+
+        Sayable s2= (name) -> {return "Hello,"+name;};
+        System.out.println(s2.say("Miss Pots"));
+    }
+}
