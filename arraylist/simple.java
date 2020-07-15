f069e03f36a9fc87120c92832c69f22ca5c8bb82
VikramVikrant <mensharetoo@gmail.com>
2020-07-15 10:32:41 +0530

    Adding arraylist

---
+++ arraylist/simple.java
@@ -0,0 +1,15 @@
+package arraylist;
+import java.util.*;
+public class simple {
+    public static void main(String[] args) {
+        List <String> colors = new ArrayList<String>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Orange");
+        colors.add("Pink");
+        colors.add("Black");
+        colors.add("White");
+        System.out.println(colors);
+    }
+}
