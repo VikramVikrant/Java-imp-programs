19ee6af1a8d7461bd6de7a2dc8635957f20f9371
VikramVikrant <mensharetoo@gmail.com>
2020-07-15 19:49:34 +0530

    Appending elements to linked list

---
+++ linkedlist/append_end.java
@@ -0,0 +1,17 @@
+package linkedlist;
+import java.util.*;
+public class append_end {
+    public static void main(String[] args) {
+        LinkedList <String> ll1 = new LinkedList<String>();
+        ll1.add("Ram");
+        ll1.add("Shyam");
+        ll1.add("Ghanshyam");
+        ll1.add("Dhanush");
+        ll1.add("Sarish");
+        System.out.println("Elements of Linked List"+ll1);
+    }
+}
+
+/**
+ * Elements of Linked List[Ram, Shyam, Ghanshyam, Dhanush, Sarish]
+ */
