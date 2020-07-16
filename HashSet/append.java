626b9d649305da6d5ebc2f187145aae2999d43de
VikramVikrant <mensharetoo@gmail.com>
2020-07-16 09:55:48 +0530

    appending elements in hashset

---
+++ HashSet/append.java
@@ -0,0 +1,19 @@
+package HashSet;
+import java.util.*;
+public class append {
+    public static void main(String[] args) {
+        HashSet<String> h1=new HashSet<String>();
+        h1.add("Red Brick");
+        h1.add("Falcon brown");
+        h1.add("Grey metal");
+        h1.add("Volcano orange");
+        h1.add("Black shadow");
+        h1.add("Dark matter");
+        h1.add("Combustion blue");
+        System.out.println("Hash Set:"+h1);
+    }
+}
+
+/**
+ * Hash Set:[Red Brick, Falcon brown, Volcano orange, Dark matter, Combustion blue, Grey metal, Black shadow]
+ */
