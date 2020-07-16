3b56c4a3438351eb7906e7cd100866b140829ba0
VikramVikrant <mensharetoo@gmail.com>
2020-07-16 15:21:45 +0530

    count the no. of key-value mapping

---
+++ Hashmap/count.java
@@ -0,0 +1,15 @@
+package Hashmap;
+import java.util.*;
+public class count {
+    public static void main(String[] args) {
+        HashMap <Integer,String> hm= new HashMap<Integer, String>();
+        hm.put(1,"you got nasty one");
+        hm.put(2,"you got shoe two");
+        hm.put(3,"you got three let me free");
+        hm.put(4,"it's sour you got four");
+        hm.put(5,"I have a knife and you fot five");
+
+        System.out.println("Size of hashmap:"+hm.size());
+    }
+}
+//Size of hashmap:5
