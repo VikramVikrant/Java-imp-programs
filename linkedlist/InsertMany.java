682e0d7697cef1bcedb1c422f50e2e38b3f27bd8
VikramVikrant <mensharetoo@gmail.com>
2020-07-16 09:39:33 +0530

    adding a list of elemnts to a existing list

--- linkedlist/InsertMany.java
+++ linkedlist/InsertMany.java
@@ -1,6 +1,6 @@
 package linkedlist;
 import java.util.*;
-public class Ading1stlast {
+public class InsertMany {
     public static void main(String[] args) {
         LinkedList <String> l1 =new LinkedList<String>();
         l1.add("Red Brick");
@@ -12,16 +12,16 @@ public class Ading1stlast {
         l1.add("Combustion blue");
         System.out.println("Original linked list:"+l1);
 
-        //adding element to the first
-        l1.addFirst("Glacier white");
-        //adding element to the last
-        l1.addLast("Glittering blue");
-        System.out.println("New Linked List:"+l1);
+        LinkedList <String> l2 = new LinkedList<String>();
+        l2.add("Plant Green");
+        l2.add("Lust Red");
+        l2.add("wood skin");
+        l1.addAll(1,l2);
+        System.out.println("After insertion of new list at index 1:>"+l1);
     }
 }
-//you can also use offerFirst instead of addFirst
-//you can also use offerLast instead of addLast
+
 /**
  * Original linked list:[Red Brick, Falcon brown, Grey metal, Volcano orange, Black shadow, Dark matter, Combustion blue]
- * New Linked List:[Glacier white, Red Brick, Falcon brown, Grey metal, Volcano orange, Black shadow, Dark matter, Combustion blue, Glittering blue]
+ * After insertion of new list at index 1:>[Red Brick, Plant Green, Lust Red, wood skin, Falcon brown, Grey metal, Volcano orange, Black shadow, Dark matter, Combustion blue]
  */
